e08b57e588b98721fffd8a725db3f0da9db3a36a
Sören Kampschroer <soerenkampschroer@gmail.com>
2018-07-30 00:08:34 +0200

    added enum for notification types, documentation

--- chrome-extension/app/scripts/content/src/Notification.js
+++ chrome-extension/app/scripts/content/src/Notification.js
@@ -1,15 +1,35 @@
 /* global Toastify */
 
+/**
+ * Notification Class
+ * Wrapper for Toastify, displays notifications
+ */
 export default class Notification {
-    display(text, type) {
+
+    /**
+     * Notification types.
+     * @readonly
+     * @enum {string}
+     */
+    static Type = {
+        SUCCESS: "success",
+        ERROR: "error"
+    };
+
+    /**
+     * Displays notification.
+     * @param {string} text 
+     * @param {Type} [type=Notification.Type.SUCCESS] type 
+     */
+    display(text, type = Notification.Type.SUCCESS) {
 
         var backgroundColor;
 
         switch (type) {
-            case "error":
+            case Notification.Type.ERROR:
                 backgroundColor = "linear-gradient(to bottom, #E53935, #D32F2F)";
                 break;
-            case "success":
+            case Notification.Type.SUCCESS:
             default:
                 backgroundColor = "linear-gradient(to bottom, #4CAF50, #43A047)";
                 break;
