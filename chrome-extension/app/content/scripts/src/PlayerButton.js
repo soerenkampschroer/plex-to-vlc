f255b5989e7b51ead4c6c5d4af7be6f17580fcfe
Sören Kampschroer <soerenkampschroer@gmail.com>
2021-07-14 16:23:19 +0200

    fixed VLC button not showing

--- chrome-extension/app/content/scripts/src/PlayerButton.js
+++ chrome-extension/app/content/scripts/src/PlayerButton.js
@@ -3,7 +3,8 @@ export default class PlayerButton {
     constructor(clickCallback) {
         this.callback = clickCallback;
         this.interval = null;
-        this.toolbarSelector = ".measuredPageHeaderToolbar-toolbar-2vtgM4";
+        this.toolbarSelector = "[class^=PrePlayActionBar]";
+        this.insertSelector = this.toolbarSelector + " button:nth-child(4)";
         this.handleButtonAdd();
         window.addEventListener("hashchange", this.handleButtonAdd.bind(this));
     }
@@ -26,9 +27,11 @@ export default class PlayerButton {
     addVlcLink() {
         if (!document.querySelector(".ptvlc-launch")) {
             var el = document.createElement("button");
-            el.classList = "ptvlc-launch ToolbarButton-toolbarButton-2i2nnD Link-link-2n0yJn Link-default-2XA2bN";
+            el.classList = "ptvlc-launch ActionButton-iconActionButton-3RWDHb ActionButton-actionButton-392O1X Button-button-1q7C1V Link-link-2WGTd7 ActionButton-medium-2--fwJ Button-button-1q7C1V Link-link-2WGTd7 Button-default-37HUnP Button-medium-21zX8X Link-default-1Q-OS9";
             el.innerHTML = "VLC";
-            document.querySelector(this.toolbarSelector).append(el);
+            var toolbar = document.querySelector(this.toolbarSelector);
+            var buttonLocation = document.querySelector(this.insertSelector);
+            toolbar.insertBefore(el, buttonLocation);
             document.querySelector(".ptvlc-launch").addEventListener("mousedown", this.callback);
         }
     }
