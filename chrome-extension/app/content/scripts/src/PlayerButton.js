3d5d8ea04f987211f3ae437c557d87e475298756
Sören Kampschroer <soerenkampschroer@gmail.com>
2023-03-21 00:01:12 +0100

    fixed player button not showing up, should be a little more robust now

--- chrome-extension/app/content/scripts/src/PlayerButton.js
+++ chrome-extension/app/content/scripts/src/PlayerButton.js
@@ -3,7 +3,7 @@ export default class PlayerButton {
     constructor(clickCallback) {
         this.callback = clickCallback;
         this.interval = null;
-        this.toolbarSelector = "[class^=PrePlayActionBar]";
+        this.toolbarSelector = "[class^='ActionButtonBar'],[class*=' ActionButtonBar'] > div";
         this.insertSelector = this.toolbarSelector + " button:nth-child(4)";
         this.handleButtonAdd();
         window.addEventListener("hashchange", this.handleButtonAdd.bind(this));
@@ -17,7 +17,7 @@ export default class PlayerButton {
 
     checkLoaded() {
         this.interval = setInterval(() => {
-            if (document.querySelector(this.toolbarSelector)) {
+            if (document.querySelector(this.toolbarSelector) !== null) {
                 this.addVlcLink();
                 clearInterval(this.interval);
             }
@@ -27,10 +27,11 @@ export default class PlayerButton {
     addVlcLink() {
         if (!document.querySelector(".ptvlc-launch")) {
             var el = document.createElement("button");
-            el.classList = "ptvlc-launch ActionButton-iconActionButton-3RWDHb ActionButton-actionButton-392O1X Button-button-1q7C1V Link-link-2WGTd7 ActionButton-medium-2--fwJ Button-button-1q7C1V Link-link-2WGTd7 Button-default-37HUnP Button-medium-21zX8X Link-default-1Q-OS9";
-            el.innerHTML = "VLC";
             var toolbar = document.querySelector(this.toolbarSelector);
             var buttonLocation = document.querySelector(this.insertSelector);
+            el.classList =  "ptvlc-launch " + buttonLocation.classList;
+            el.innerHTML = "VLC";
+            console.log(el, buttonLocation);
             toolbar.insertBefore(el, buttonLocation);
             document.querySelector(".ptvlc-launch").addEventListener("mousedown", this.callback);
         }
