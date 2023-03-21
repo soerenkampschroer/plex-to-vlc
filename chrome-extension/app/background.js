bed76fa97837cf17ab2e5f32765c78b447231956
Sören Kampschroer <soerenkampschroer@gmail.com>
2023-03-21 01:44:21 +0100

    new compiler

--- chrome-extension/app/background.js
+++ chrome-extension/app/background.js
@@ -15,16 +15,13 @@ __webpack_require__.r(__webpack_exports__);
 class Settings {
   markItemsPlayed = true;
   player = "default";
-
   constructor() {}
-
   get() {
     return {
       "markItemsPlayed": this.markItemsPlayed,
       "player": this.player
     };
   }
-
   save() {
     let settings = {
       markItemsPlayed: this.markItemsPlayed,
@@ -32,37 +29,30 @@ class Settings {
     };
     chrome.storage.local.set(settings);
   }
-
   load() {
     return new Promise((resolve, reject) => {
       chrome.storage.local.get(null, items => {
         if (chrome.runtime.lastError) {
           return reject(chrome.runtime.lastError);
         }
-
         let saveSettings = false;
-
         if (items.markItemsPlayed === undefined) {
           items.markItemsPlayed = this.markItemsPlayed;
           saveSettings = true;
         }
-
         if (!items.player === undefined) {
           items.player = this.player;
           saveSettings = true;
         }
-
         if (saveSettings) {
           this.save(items);
         }
-
         this.markItemsPlayed = items.markItemsPlayed;
         this.player = items.player;
         resolve(this);
       });
     });
   }
-
 }
 
 /***/ })
@@ -132,47 +122,39 @@ var __webpack_exports__ = {};
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _Settings_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Settings.js */ "./app/background/scripts/src/Settings.js");
 
-
 class Main {
   /**
    * This is the host app id.
    */
   static HOST_NAME = "com.soerenkampschroer.plextovlc";
+
   /**
    * Minumum companion app version.
    */
-
   static ACCEPTABLE_HOST_VERSION = "1.4.0";
-
   constructor() {
     chrome.runtime.onMessage.addListener(this.handleMessage.bind(this));
     chrome.runtime.onInstalled.addListener(this.handleOnInstalled.bind(this));
   }
-
   handleMessage(request, caller, callback) {
     this.handleMessageWrapper(request, caller, callback).then(callback);
     return true;
   }
-
   async handleMessageWrapper(request, caller, callback) {
-    let settings = new _Settings_js__WEBPACK_IMPORTED_MODULE_0__.default();
+    let settings = new _Settings_js__WEBPACK_IMPORTED_MODULE_0__["default"]();
     await settings.load();
-
     switch (request.type) {
       case "playback":
         request.player = settings.get().player;
         this.openNativeMessagePort(request, this.onDisconnected.bind(this), this.onMessage.bind(this));
         return true;
-
       case "getSettings":
         return settings.get();
-
       case "saveSettings":
         settings.markItemsPlayed = request.settings.markItemsPlayed;
         settings.player = request.settings.player;
         settings.save(request.settings);
         return true;
-
       case "hostVersionCheck":
         this.openNativeMessagePort({
           type: "version"
@@ -180,50 +162,42 @@ class Main {
         return true;
     }
   }
-
-  handleOnInstalled() {// console.log('checking host app version');
+  handleOnInstalled() {
+    // console.log('checking host app version');
     // this.openNativeMessagePort({type: "version"}, this.onDisconnected.bind(this), this.onCheckVersionMessage.bind(this));
   }
-
   onCheckVersionMessage(request, port) {
     if (!request.version || !this.compareVersion(Main.ACCEPTABLE_HOST_VERSION, request.version)) {
       this.displayHostUpdateDialog();
     }
-
     port.disconnect();
   }
-
   compareVersion(ver1, ver2) {
     ver1 = ver1.split(".").map(s => s.padStart(10)).join(".");
     ver2 = ver2.split(".").map(s => s.padStart(10)).join(".");
     return ver1 <= ver2;
   }
-
   openNativeMessagePort(request, onDisconnectedCallback, onMessageCallback) {
     let port = chrome.runtime.connectNative(Main.HOST_NAME);
     port.onDisconnect.addListener(onDisconnectedCallback);
     port.onMessage.addListener(onMessageCallback);
     port.postMessage(request);
   }
-
   async onMessage(request, port) {
-    let settings = new _Settings_js__WEBPACK_IMPORTED_MODULE_0__.default();
+    let settings = new _Settings_js__WEBPACK_IMPORTED_MODULE_0__["default"]();
     await settings.load();
     request.markItemsPlayed = settings.get().markItemsPlayed;
     this.sendMessageToContentScript(request);
     port.disconnect();
   }
-
   onDisconnected() {
     if (chrome.runtime.lastError.message == "Specified native messaging host not found." || chrome.runtime.lastError.message == "Access to the specified native messaging host is forbidden.") {
       this.displayHostUpdateDialog();
     }
   }
-
   displayHostUpdateDialog() {
     this.sendMessageToContentScript("installHost");
   }
-
   sendMessageToContentScript(message) {
     chrome.tabs.query({
       active: true
@@ -231,9 +205,7 @@ class Main {
       chrome.tabs.sendMessage(tabs[0].id, message);
     });
   }
-
 }
-
 new Main();
 })();
 
